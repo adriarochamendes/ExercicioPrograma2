d0b6d2ae704ec58601fa750aef260844fd042c36
Laura Monteiro <lauradm1@al.insper.edu.br>
2026-05-01 17:51:47 -0300

    Questão 1: Rolar dados

---
+++ funcoes.py
@@ -0,0 +1,12 @@
+#questao1
+
+import random
+
+def rolar_dados(qtd):
+    lista = []
+    
+    for i in range(qtd):
+        dado = random.randint(1, 6)
+        lista.append(dado)
+    
+    return lista
